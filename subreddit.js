b6224810e47cf0cb202c656753807f6eaa87c270
Olivier Truong <ohtruong@gmail.com>
2012-04-09 13:31:32 -0700

    Code cleanup

--- subreddit.js
+++ subreddit.js
@@ -1,7 +1,8 @@
 var $bar = $('.sr-list');
 var $customBar = $('.flat-list.sr-bar.hover:eq(1)');
-var subreddits = ['nba', 'madmen', 'gameofthrones', 'programming', 'python', 'starcraft', 'malefashionadvice', 
-                  'wow', 'arduino', 'videos'];
+var subreddits = ['nba', 'madmen', 'gameofthrones', 'programming', 
+                  'python', 'starcraft', 'malefashionadvice', 'wow', 
+                  'arduino', 'videos'];
 var i, len;
 $customBar.empty();
 $bar.children('span.separator:eq(1)').remove();
