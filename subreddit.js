d20e3f021723e371d78634ed839a70b59552bd84
Olivier Truong <ohtruong@gmail.com>
2012-04-13 13:38:29 -0700

    Commented code

--- subreddit.js
+++ subreddit.js
@@ -1,28 +1,36 @@
+// Fetch local pins from browser
 var pins = localStorage.getItem('pins');
 if (pins) {
   pins = JSON.parse(pins);
 } else {
   pins = [];
 }
-var $bar = $('.sr-list');
-var $customBar = $('.flat-list.sr-bar.hover:eq(1)');
-var subreddits = ['nba', 'madmen', 'gameofthrones', 'programming', 
-                  'python', 'starcraft', 'malefashionadvice', 'wow', 
-                  'arduino', 'videos'];
-var i, len;
+
+var $bar = $('.sr-list'); // Entire top bar
+var $customBar = $('.flat-list.sr-bar.hover:eq(1)'); // Personal user bar
+
+// Clear the bar of default subreddits
 $customBar.empty();
 $bar.children('span.separator:eq(1)').remove();
 $bar.children('.flat-list.sr-bar.hover:eq(2)').remove();
+
+// Add all user pins to subreddit bar
+var i, len;
 for (i = 0, len = pins.length; i < len; ++i) {
   if (i != 0) {
     $customBar.append('<li><span class="separator" id="'+pins[i]+'">-</span>'+
-                    '<a href="http://reddit.com/r/'+pins[i]+'/" id="'+pins[i]+'">'+pins[i]+'</a></li>');
+                      '<a href="http://reddit.com/r/'+pins[i]+'/" id="'+pins[i]+'">'+
+                      pins[i]+'</a></li>');
   } else {
-    $customBar.append('<li><a href="http://reddit.com/r/'+pins[i]+'/" id="'+pins[i]+'">'+pins[i]+'</a></li>');
+    $customBar.append('<li><a href="http://reddit.com/r/'+pins[i]+'/" id="'+pins[i]+
+                      '">'+pins[i]+'</a></li>');
   }
 }
-var name = $('span.redditname').children('a').text();
-var $subscribe = $('div.titlebox span.fancy-toggle-button');
+
+var name = $('span.redditname').children('a').text(); // Name of current subreddit
+var $subscribe = $('div.titlebox span.fancy-toggle-button'); // Element containing the subscribe/pin actions
+
+// Check for pin status and create pin/unpin action accordingly.
 if ($.inArray(name, pins) == -1) {
   $subscribe.after('<span class="fancy-toggle-button toggle">'+
                    '<a class="option active add" id="pin" href="#"'+ 
@@ -32,6 +40,8 @@ if ($.inArray(name, pins) == -1) {
                    '<a class="option active remove" id="pin" href="#"'+ 
                    'tabindex="100")">unpin</a></span>');
 }
+
+// Handle pin/unpin action
 $pin = $('span.fancy-toggle-button a#pin');
 $pin.click(function() {
   event.preventDefault();
@@ -43,9 +53,11 @@ $pin.click(function() {
     localStorage.setItem('pins', JSON.stringify(pins));
     if (pins.length > 1) {
       $customBar.append('<li><span class="separator" id="'+name+'">-</span>'+
-                        '<a href="http://reddit.com/r/'+name+'/" id="'+name+'">'+name+'</a></li>');
+                        '<a href="http://reddit.com/r/'+name+'/" id="'+name+
+                        '">'+name+'</a></li>');
     } else {
-      $customBar.append('<li><a href="http://reddit.com/r/'+name+'/" id="'+name+'">'+name+'</a></li>');
+      $customBar.append('<li><a href="http://reddit.com/r/'+name+'/" id="'+
+                        name+'">'+name+'</a></li>');
     }
     $pin.removeClass('add').addClass('remove').text('unpin');
   } else {
