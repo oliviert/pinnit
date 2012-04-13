119ee3801cb91eefd2007bdc2573e3ac63b025c5
Olivier Truong <ohtruong@gmail.com>
2012-04-13 15:01:25 -0700

    Add message when pin bar is empty

--- pin.js
+++ pin.js
@@ -15,16 +15,21 @@ $bar.children('span.separator:eq(1)').remove();
 $bar.children('.flat-list.sr-bar.hover:eq(2)').remove();
 
 // Add all user pins to subreddit bar
-var i, len;
-for (i = 0, len = pins.length; i < len; ++i) {
-  if (i != 0) {
-    $customBar.append('<li><span class="separator" id="'+pins[i]+'">-</span>'+
-                      '<a href="http://reddit.com/r/'+pins[i]+'/" id="'+pins[i]+'">'+
-                      pins[i]+'</a></li>');
-  } else {
-    $customBar.append('<li><a href="http://reddit.com/r/'+pins[i]+'/" id="'+pins[i]+
-                      '">'+pins[i]+'</a></li>');
+if (pins.length > 0) {
+  var i, len;
+  for (i = 0, len = pins.length; i < len; ++i) {
+    if (i != 0) {
+      $customBar.append('<li><span class="separator" id="'+pins[i]+'">-</span>'+
+          '<a href="http://reddit.com/r/'+pins[i]+'/" id="'+pins[i]+'">'+
+          pins[i]+'</a></li>');
+    } else {
+      $customBar.append('<li><a href="http://reddit.com/r/'+pins[i]+'/" id="'+pins[i]+
+          '">'+pins[i]+'</a></li>');
+    }
   }
+} else {
+  $customBar.append('<li style="color: gray;">You have no pins. Pin a subreddit '+
+                    'by clicking the green Pin button in the sidebar</li>');
 }
 
 var name = $('span.redditname').children('a').text(); // Name of current subreddit
@@ -49,6 +54,9 @@ $pin.click(function() {
     if ($.inArray(name, pins) > -1) {
       return;
     }
+    if (pins.length == 0) {
+      $customBar.empty();
+    }
     pins.push(name);
     localStorage.setItem('pins', JSON.stringify(pins));
     if (pins.length > 1) {
@@ -71,5 +79,9 @@ $pin.click(function() {
     $customBar.children('li').children('a#'+name).remove();
     $customBar.children('li').children('span#'+name).remove();
     $pin.removeClass('remove').addClass('add').text('pin');
+    if (pins.length == 0) {
+      $customBar.append('<li style="color: gray;">You have no pins. Pin a subreddit '+
+                        'by clicking the green Pin button in the sidebar</li>');
+    }
   }
 });
